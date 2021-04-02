ba6029803a29622b6ea51bb9060fbdef5866ad97
Lorena C <lorena@eunknown.org>
2021-04-01 19:44:42 -0700

    add weather api

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,10 @@
 class MainController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=90210&distance=5&API_KEY=8ACBE8C4-BE62-4D88-9EBD-E56CF0CE5107'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
   end
 end
