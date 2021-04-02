032ab1ddc1d367e08293ada01bcd591822991936
Lorena C <lorena@eunknown.org>
2021-04-01 20:33:46 -0700

    add if statement for color

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,5 +6,37 @@ class MainController < ApplicationController
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output = JSON.parse(@response)
+
+    if @output.empty?
+      @final_output = 'Error'
+    elseif !@output
+    @final_output = 'Error'
+    else
+      @final_output = @output[0]['AQI']
+    end 
+
+    if @final_output ==="Error"
+      @api_color = 'gray'
+    elsif @final_output <= 50
+      @api_color = 'green'
+    elsif @final_output >= 51 && final_output <= 100
+      @api_color = 'yellow'
+    elsif @final_output >= 101 && final_output <= 150
+      @api_color = 'orange'
+    elsif @final_output >= 151 && final_output <= 200
+      @api_color = 'red'
+    elsif @final_output >= 201 && final_output <= 300
+      @api_color = 'purple'
+    elsif @final_output >= 301 && final_output <= 500
+      @api_color = 'maroon'
+
+
+
+      
+    end
+
+    
   end
+
+  
 end
